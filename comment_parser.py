13b0aa98995ac79fa1aae3d10c1f97806627e290
Sergey Khakhin <sergekhakhin@gmail.com>
2020-07-16 21:18:09 +0300

    Added comment_parser.py

---
+++ comment_parser.py
@@ -0,0 +1,5 @@
+from issues import get_opened_issues, get_issue_comments, leave_comment
+
+for issue_id in get_opened_issues():
+    if not get_issue_comments(issue_id):
+        leave_comment(issue_id, 'В работе.', author_id=6)
