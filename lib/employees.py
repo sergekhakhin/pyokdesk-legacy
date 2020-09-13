4b8d5e71c1163406a3a79ec476278a85c30e4583
Sergey Khakhin <sergekhakhin@gmail.com>
2020-09-13 23:27:18 +0300

    Removed 'sok' from the dict. Refactored functions.

--- lib/employees.py
+++ lib/employees.py
@@ -4,26 +4,25 @@ employee_dict = {
     'kis': 1,
     'mon': 3,
     'kha': 6,
-    'sok': 7,
     'per': 8
 }
 
 
-def get_employee_id(ref):
+def get_employee_id_by_ref(ref):
     try:
         return employee_dict[ref]
     except KeyError:
         print("[ ERROR ] Не удалось найти сотрудника")
 
 
-def get_random_employee_id():
-    id_list = list(employee_dict.values())
-    id_list.remove(1)
-    return random.choice(id_list)
-
-
-def get_employee_ref(employee_id):
+def get_employee_ref_by_id(employee_id):
     try:
         return [k for k, v in employee_dict.items() if v == employee_id]
     except KeyError:
         print("[ ERROR ] Не удалось найти сотрудника по ID")
+
+
+def get_random_employee_id():
+    id_list = list(employee_dict.values())
+    id_list.remove(1)
+    return random.choice(id_list)
