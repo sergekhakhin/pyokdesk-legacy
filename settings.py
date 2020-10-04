c36b380034814eee7f52d3509d65c2b28f925f08
Sergey Khakhin <sergekhakhin@gmail.com>
2020-10-04 13:53:47 +0300

    Minor change to imports

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-import os
+from os import getenv
 
 api_uri = 'https://uvercom.okdesk.ru/api/v1'
-api_token = os.getenv('OKDESK_API_KEY')
+api_token = getenv('OKDESK_API_KEY')
 token = {'api_token': api_token}
