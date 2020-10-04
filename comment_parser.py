d8ecf4669700cb98ee0e67cde5a1f5ac8db8e0ab
Sergey Khakhin <sergekhakhin@gmail.com>
2020-10-04 13:52:24 +0300

    Comment text as a variable

--- comment_parser.py
+++ comment_parser.py
@@ -1,5 +1,7 @@
 from lib.issues import get_issue_list_by_status, get_issue_comments, add_comment
 
-for issue_id in get_issue_list_by_status('opened'):
+comment = "В работе."
+
+for issue_id in get_issue_list_by_status(['opened', 'repair']):
     if not get_issue_comments(issue_id):
-        add_comment(issue_id, 'В работе.', author_id=6, public=True)
+        add_comment(issue_id, comment, author_id=6, public=True)
